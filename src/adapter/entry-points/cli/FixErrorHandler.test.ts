65aaf6be7e35bd619771c41eff5b0e2e3458f1b3
HiromiShikata <shikata.hiromi@gmail.com>
2023-06-09 13:33:49 +0000

    feat: add cousion about domain entity to context

--- src/adapter/entry-points/cli/FixErrorHandler.test.ts
+++ src/adapter/entry-points/cli/FixErrorHandler.test.ts
@@ -4,7 +4,7 @@ jest.setTimeout(10 * 60 * 1000);
 describe('FixErrorHandler', () => {
   test('success', async () => {
     const fixErrorHandler = await fixErrorCliHandler(
-      `Please fix this error.`,
+      `Please fix this error. domain/entities must not be updated. I think logic inside other layer is wrong.`,
       '/home/hiromi/git/umino/Auto-GPT-Programming-TypeScript/sample-project',
       'npm run build && npm run test',
       'gpt-4',
