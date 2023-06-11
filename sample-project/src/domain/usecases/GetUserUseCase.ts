eb5091e9e7d898ff82859d5492cb255eeba00fe6
HiromiShikata <shikata.hiromi@gmail.com>
2023-06-11 21:49:20 +0000

    feat: fix error in sample-project

--- sample-project/src/domain/usecases/GetUserUseCase.ts
+++ sample-project/src/domain/usecases/GetUserUseCase.ts
@@ -7,7 +7,7 @@ export class GetUserUseCase {
   execute = async (id: string): Promise<User> => {
     if (!id) throw new Error('id is required');
     const user: User | null = await this.userRepository.getById(id);
-    if (user) throw new Error(`${id} is not found`);
+    if (!user) throw new Error(`${id} is not found`);
     return user;
   };
 }
