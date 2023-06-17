4b4347315ac05585a1f11836ce50e3a13f4d66d0
HiromiShikata <shikata.hiromi@gmail.com>
2023-06-17 00:36:27 +0000

    feat: build
    npm run build

---
+++ bin/domain/usecases/FixErrorUseCase.js
@@ -0,0 +1,126 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.FixErrorUseCase = void 0;
+class FixErrorUseCase {
+    constructor(fileRepository, bashExecutor, patchCreator, dateTimeRepository) {
+        this.fileRepository = fileRepository;
+        this.bashExecutor = bashExecutor;
+        this.patchCreator = patchCreator;
+        this.dateTimeRepository = dateTimeRepository;
+        this.run = async (context, projectRootPath, testCommand, commandToSeeProjectStructure) => {
+            let relatedSourceFiles = [];
+            // const branchName = this.createBranchName(context);
+            //     const createBranchResult =await  this.bashExecutor.execute(
+            // `cd ${projectRootPath}
+            // && git switch -c ${branchName}
+            // `)
+            for (let i = 0; i < 60; i++) {
+                console.log(`${i + 1}th try`);
+                const { stderr, stdout, exitStatusCode } = await this.bashExecutor.execute(`cd ${projectRootPath} && ${commandToSeeProjectStructure} && ${testCommand}`);
+                if (exitStatusCode === 0) {
+                    console.log(`stdout: ${stdout}`);
+                    console.log('✨🎉Finished to fix error🎉✨');
+                    return;
+                }
+                console.log(`🔥Failed the test🔥`);
+                const errorMessage = `
+# executed command
+${testCommand}
+
+# stdout
+${stdout}
+
+# stderr
+${stderr
+                    .split('\n')
+                    .filter((m) => !m.includes('Debugger listening on ws://') &&
+                    !m.includes('For help, see: https://nodejs.org/en/docs/inspector') &&
+                    !m.includes('Waiting for the debugger to disconnect...') &&
+                    !m.includes('Debugger attached.'))
+                    .join('\n')}
+`;
+                console.log(errorMessage);
+                const res = await this.patchCreator.fix(context, relatedSourceFiles, errorMessage);
+                console.log(`Thought: ${res.thoughtJapanese}
+Commit message: ${res.commitMessage ?? 'null'}
+File paths to read: ${res.filePathsToRead.join(', ')}
+Unified diff formatted patch: 
+${res.unifiedDiffFormattedPatch ?? 'null'}
+`);
+                if (res.unifiedDiffFormattedPatch) {
+                    const patchApplyCommand = `cd ${projectRootPath} && patch --ignore-whitespace --force -p1 <<'EOF'
+        ${res.unifiedDiffFormattedPatch}
+        EOF
+        `;
+                    const patchApplyResult = await this.bashExecutor.execute(patchApplyCommand);
+                    if (patchApplyResult.exitStatusCode === 0) {
+                        // const gitOperationResult =             await this.bashExecutor.execute(`cd ${projectRootPath}
+                        // && git add -A
+                        // && git commit -m "${res.commitMessage}"
+                        // && git push
+                        // && gh pr create --title "${res.commitMessage}" --body "${res.thoughtJapanese}" --repo ${process.env.GITHUB_REPOSITORY}
+                        // `);
+                        //           if(gitOperationResult.exitStatusCode === 0){
+                        //               await this.bashExecutor.execute(`
+                        // cd ${projectRootPath}
+                        // && git checkout -b ${gitOperationResult.stdout}
+                        //               `)
+                        //
+                        //           }
+                    }
+                    else {
+                        console.error(`Failed to apply patch.
+exitStatusCode: ${patchApplyResult.exitStatusCode}
+stdout: ${patchApplyResult.stdout}
+stderr: ${patchApplyResult.stderr}
+command:
+${patchApplyCommand}
+
+patchApplyResult:
+${JSON.stringify(patchApplyResult)}`);
+                    }
+                }
+                relatedSourceFiles = await this.fileRepository.read(res.filePathsToRead, projectRootPath);
+            }
+            throw new Error('Failed to fix error');
+        };
+        this.createBranchName = (context) => {
+            const now = this.dateTimeRepository.now();
+            const dateTimeText = this.dateTimeRepository.yyyyMMddHHmm(now);
+            const illegalCharacters = [
+                '~',
+                '^',
+                ':',
+                '\\',
+                '*',
+                '?',
+                '[',
+                ']',
+                '.',
+                '/',
+                '@{',
+                '.lock',
+                String.fromCharCode(127),
+            ];
+            for (let i = 0; i < 32; i++) {
+                illegalCharacters.push(String.fromCharCode(i));
+            }
+            let newStr = context.trim().toLowerCase();
+            illegalCharacters.forEach((char) => {
+                const regex = new RegExp(`\\${char}`, 'g');
+                newStr = newStr.replace(regex, '-');
+            });
+            newStr = newStr.replace(/\/+/g, '/');
+            newStr = newStr.replace(/^\/+|\/+$/g, '');
+            const branchNameSuffix = newStr
+                .split(' ')
+                .slice(0, 5)
+                .join('-')
+                .substring(0, 20);
+            const branchName = `${dateTimeText}-${branchNameSuffix}`;
+            return branchName;
+        };
+    }
+}
+exports.FixErrorUseCase = FixErrorUseCase;
+//# sourceMappingURL=FixErrorUseCase.js.map
